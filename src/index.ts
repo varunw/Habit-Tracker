e187818be846272b189ce53f647d38b34fc46709
Varun Waykar <waykarvarun@gmail.com>
2025-11-19 18:18:28 +0530

    Single line commit

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { app } from "./server.ts";
 import { env } from "../env.ts";
 
 app.listen(env.PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port 3000: ${env.PORT}`);
 });
